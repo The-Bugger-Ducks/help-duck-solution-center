d8e0ebc0ecc7e745330acdd344ce0362f3c0100a
thhenrique <henriquethiago298@gmail.com>
2022-05-24 01:00:30 -0300

    created methods to increment up and down vote in solution

--- src/main/java/com/helpduck/helpducksolutioncenter/model/solutionComment/SolutionUpdater.java
+++ src/main/java/com/helpduck/helpducksolutioncenter/model/solutionComment/SolutionUpdater.java
@@ -9,4 +9,12 @@ public class SolutionUpdater {
 	public void update(SolutionComment solution, SolutionComment updatedSolution) {
 		solution.setSolutionComment((updatedSolution.getSolutionComment()));
 	}
+
+	public void incrementUpVote(SolutionComment solution) {
+		solution.setUpVote(solution.getUpVote() + 1);
+	}
+
+	public void incrementDownVote(SolutionComment solution) {
+		solution.setDownVote(solution.getDownVote() + 1);
+	}
 }
